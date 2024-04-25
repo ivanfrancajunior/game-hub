f6efa78f7a319ab34464bec5d7a8a0f969ac10f4
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-25 08:48:32 -0300

    add getCroppedImageUrl function

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
 type Props = {
   game: Game;
@@ -13,7 +14,7 @@ const GameCard = ({ game }: Props) => {
       borderRadius={8}
       overflow={"hidden"}
     >
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
---
+++ src/services/getCroppedImageUrl.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return url;
+  const target = "media/";
+  const source = url;
+  const index = source.indexOf(target) + target.length;
+  return source.slice(0, index) + "crop/600/400/" + source.slice(index);
+};
+
+export default getCroppedImageUrl;
