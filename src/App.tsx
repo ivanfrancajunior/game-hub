47b2bbce313dd8f55810ce32f24431be88361d9e
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-25 00:32:11 -0300

    add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <div>
-      <Button>Hello</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" " main"`,
+          lg: `"nav nav" "asside main"`,
+        }}
+      >
+        <GridItem
+          area='nav'
+          bg='blueviolet'
+        >
+          Nav
+        </GridItem>
+        <Show above='lg'>
+          <GridItem
+            area='asside'
+            bg='teal'
+          >
+            Asside
+          </GridItem>
+        </Show>
+        <GridItem
+          area='main'
+          bg='tomato'
+        >
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 };
