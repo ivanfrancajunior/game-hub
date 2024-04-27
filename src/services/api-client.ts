71b3caced1da5dc386d60f87db31fb5590c0fd55
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-27 13:44:52 -0300

    add APIClient class

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,25 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
-  baseURL: "https://api.rawg.io/api/",
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+class APIClient<T> {
+  constructor(public endpoint: string) {}
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data)
+  };
+}
+
+export default APIClient;
